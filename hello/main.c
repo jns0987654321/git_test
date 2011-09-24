dff46c7362b044e14857016cd12ed2b56ecbe102
Junior Samaroo <jsamaroo@Junior-Samaroos-MacBook-Pro.local>
2011-09-24 15:13:38 -0400

    ad 123

--- hello/main.c
+++ hello/main.c
@@ -12,7 +12,7 @@ int main (int argc, const char * argv[])
 {
 
     // insert code here...
-    printf("Hello, World! lol 1234 \n");
+    printf("Hello, World! lol 1234 123 \n");
     return 0;
 }
 
