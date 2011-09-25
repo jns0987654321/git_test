c14c8fd91cef20ab6cf498712a98298ea572e4e3
Junior Samaroo <jsamaroo@Junior-Samaroos-MacBook-Pro.local>
2011-09-25 15:58:38 -0400

    tes pointer

--- hello/main.c
+++ hello/main.c
@@ -11,6 +11,13 @@
 int main (int argc, const char * argv[])
 {
 
+    int word[3] = {1,2,3};
+  //  int i=0;
+    printf("%lu\n", sizeof(word));
+  //  for(i=0; i<sizeof word; i++)
+    //    printf("%c\n", word[i]);
+    printf("lol %p\n", (word+1));
+    
     // insert code here...
     printf("Hello, World! lol 1234 123 dsfsfs \n");
     return 0;
